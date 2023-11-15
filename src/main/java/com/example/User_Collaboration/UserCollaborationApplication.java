60db1ca0e23da8fb9749727d713ce5acfaad4ca0
krish1770 <rameshkrishnaprasath@gmail.com>
2023-11-15 17:57:49 +0530

    feign client

--- src/main/java/com/example/User_Collaboration/UserCollaborationApplication.java
+++ src/main/java/com/example/User_Collaboration/UserCollaborationApplication.java
@@ -2,8 +2,10 @@ package com.example.User_Collaboration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class UserCollaborationApplication {
 
 	public static void main(String[] args) {
